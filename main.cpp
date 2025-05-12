ccec581945ab9a95f7a3b54ee2f37b64286abb9d
student <student@p.lodz.pl>
2025-05-12 14:08:50 +0200

    Commite message

--- main.cpp
+++ main.cpp
@@ -32,6 +32,11 @@ int main(int argc, char* argv[]) {
                 printf("%d / %d = %f\n",number1,number2,result_of_dividing);
             }
         }
+        if (!strcmp("mul", argv[2])) {
+            result = number1 * number2;
+            printf("%d * %d = %d\n", number1, number2, result);
+            return 0;
+        }
     }
 
     usage(argv[0]);
